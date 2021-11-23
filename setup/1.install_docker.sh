7e2279d16ee8bdf9178ba936b722a01e4508a6fd
Dayne Broderson <broderson@gmail.com>
2021-11-23 13:34:25 +0700

    yaking portainer installer

--- setup/1.install_docker.sh
+++ setup/1.install_docker.sh
@@ -3,16 +3,16 @@
 SCRIPT_DIR="$(dirname "$0")"
 source $SCRIPT_DIR/../lib/common.sh
 
-yak "installing docker"
 
 got_command docker
 if [ $? -eq 0 ]; then
   yak "docker already installed"
 else
+  yak "installing docker"
   require_command curl
   check_internet
   curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
   sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
 fi
 
-echo "Remember to logoff/reboot for the changes to take effect."
+yak "Remember to logoff/reboot for the changes to take effect."
